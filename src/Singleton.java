9131f406cb27deddf7465aaa00512d18ccc66642
qinaoyun <2509376331@qq.com>
2017-12-24 11:10:27 +0800

    Java并发编程实战 马士兵并发教程学习 线程安全的单例模式

---
+++ src/Singleton.java
@@ -0,0 +1,53 @@
+/**
+ * Description:延迟初始化占位（Holder）类模式（推荐） 的单例模式
+ *
+ * @author qinaoyun
+ * Date: 2017-12-24
+ * Time: 10:44
+ * <p>
+ * 线程安全的单例模式：
+ * <p>
+ * 阅读文章：http://www.cnblogs.com/xudong-bupt/p/3433643.html
+ * <p>
+ * 更好的是采用下面的方式，既不用加锁，也能实现懒加载
+ * @author qinaoyun
+ */
+/**
+ * 线程安全的单例模式：
+ *
+ * 阅读文章：http://www.cnblogs.com/xudong-bupt/p/3433643.html
+ *
+ * 更好的是采用下面的方式，既不用加锁，也能实现懒加载
+ *
+ * @author qinaoyun
+ */
+
+import java.util.Arrays;
+
+public class Singleton {
+
+    private Singleton() {
+        System.out.println("single");
+    }
+
+    private static class Inner {
+        private static Singleton s = new Singleton();
+    }
+
+    public static Singleton getSingle() {
+        return Inner.s;
+    }
+
+    public static void main(String[] args) {
+        Thread[] ths = new Thread[200];
+        for (int i = 0; i < ths.length; i++) {
+            ths[i] = new Thread(() -> {
+                System.out.println(Singleton.getSingle().hashCode());
+            });
+        }
+
+        Arrays.asList(ths).forEach(o -> o.start());
+    }
+
+}
+
